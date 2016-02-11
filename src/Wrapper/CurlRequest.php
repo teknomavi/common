477a4bb75fbf3934e4483219628a261ed341d8b9
Göktuğ ÖZTÜRK <goktug.ozturk@gmail.com>
2016-02-12 01:13:10 +0200

    Curl wrapper

---
+++ src/Wrapper/CurlRequest.php
@@ -0,0 +1,32 @@
+<?php
+namespace Teknomavi\Common\Wrapper;
+
+class CurlRequest implements HttpRequestInterface
+{
+    private $handle = null;
+
+    public function __construct($url = null)
+    {
+        $this->handle = curl_init($url);
+    }
+
+    public function setOption($name, $value)
+    {
+        curl_setopt($this->handle, $name, $value);
+    }
+
+    public function execute()
+    {
+        return curl_exec($this->handle);
+    }
+
+    public function getInfo($name = null)
+    {
+        return curl_getinfo($this->handle, $name);
+    }
+
+    public function close()
+    {
+        curl_close($this->handle);
+    }
+}
